8fbcb3e17cdd723241d6c28e9f2e8c9535577fe0
qlswe <semengladysev525@gmail.com>
2024-03-17 23:08:03 +0500

    Fixed app.

--- CreateZip.bat
+++ CreateZip.bat
@@ -1,7 +1,7 @@
 @echo off
 mkdir Zip
 
-xcopy BotGDX\bin\Release\net6.0-windows\ Zip /s /e /y
+xcopy BotGDX\bin\Debug\net6.0-windows\ Zip /s /e /y
 xcopy BotGDX.Fps\bin\Release\ Zip /s /e /y
 
 pause
